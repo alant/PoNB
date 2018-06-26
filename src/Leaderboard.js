c8c66d777bbf4b83b92dad0204cd18e8343a0d00
alant <alan.htang@gmail.com>
2018-06-26 02:17:16 -0700

    fixed bug that caused leaderboard to be down for couple of days :(

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -7,20 +7,20 @@ class Leaderboard extends Component {
     super();
     this.state = {
       nebPay: null,
-      to: "",//contract address
+      to: '', //contract address
       members: [],
       fetchInProgress: true
-    }
+    };
   }
   componentWillMount() {
-    this.setState({nebPay: new NebPay()});
-    this.setState({to: this.props.contractAddr});
+    this.setState({ nebPay: new NebPay() });
+    this.setState({ to: this.props.contractAddr });
   }
   promiseSimulateCall(to, value, callFunction, callArgs) {
     return new Promise((resolve, reject) => {
       this.state.nebPay.simulateCall(to, value, callFunction, callArgs, {
         listener: (resp) => {
-          console.log("====>" + JSON.stringify(resp));
+          console.log('====>' + JSON.stringify(resp));
           // if (resp == null || resp.execute_err !== "") {
           //   reject(Error("promiseSimulateCall broke"));
           // } else {
@@ -33,60 +33,74 @@ class Leaderboard extends Component {
   }
   componentDidMount() {
     // console.log("=Leaderboard contract addr: " + this.props.contractAddr);
-    var value = "0";
-    var callFunction = "getTeam";
-    var callArgs = "[]";
-    this.promiseSimulateCall(this.state.to, value, callFunction, callArgs)
-    .then(resp => {
-      console.log("=pSCall1 result:" + JSON.stringify(resp));
-      var result = JSON.parse(resp.result);
-      var pArray = result.members.map((addr) => {
-        var value = "0";
-        var callFunction = "getBalance";
-        var callArgs = "[\"" + addr + "\"]";
-        return this.promiseSimulateCall(this.state.to, value, callFunction, callArgs);
-      });
-      var promises = Promise.all(pArray);
-      promises.then(results => {
-        console.log("==promiseall:" + JSON.stringify(results));
-        var stateMembers = [];
-        for (var i = 0; i < result.members.length; i++)
-        {
-          var bigB = JSON.parse(results[i].result) / (10**18);
-          // bigB = bigB.dividedBy(new BigNumber("1000000000000000000"));
-          stateMembers.push({ addr: result.members[i], balance: bigB.toString() });
+    var value = '0';
+    var callFunction = 'getTeam';
+    var callArgs = '[]';
+    this.promiseSimulateCall(this.state.to, value, callFunction, callArgs).then(
+      (resp) => {
+        console.log('=pSCall1 result:' + JSON.stringify(resp));
+        if (resp.result) {
+          var result = JSON.parse(resp.result);
+          var pArray = result.members.map((addr) => {
+            var value = '0';
+            var callFunction = 'getBalance';
+            var callArgs = '["' + addr + '"]';
+            return this.promiseSimulateCall(
+              this.state.to,
+              value,
+              callFunction,
+              callArgs
+            );
+          });
+          var promises = Promise.all(pArray);
+          promises.then((results) => {
+            var stateMembers = [];
+            for (var i = 0; i < result.members.length; i++) {
+              console.log('==promiseall:' + JSON.stringify(results[i]));
+              var bigB = JSON.parse(results[i].result) / 10 ** 18;
+              // bigB = bigB.dividedBy(new BigNumber("1000000000000000000"));
+              stateMembers.push({
+                addr: result.members[i],
+                balance: bigB.toString()
+              });
+            }
+            this.setState({ members: stateMembers });
+            this.setState({ fetchInProgress: false });
+          });
+        } else {
+          console.log("==> =pSCall1 result empty!");
+          window.location.reload();
         }
-        this.setState({members: stateMembers});
-        this.setState({fetchInProgress: false});
-      });
-    });
-    
+      }
+    );
   }
   render() {
     const Members = this.state.members.map((member, i) => {
-      return (<tr key={i}><td>{member.addr}</td><td>{member.balance}</td></tr>);
+      return (
+        <tr key={i}>
+          <td>{member.addr}</td>
+          <td>{member.balance}</td>
+        </tr>
+      );
     });
     return (
       <div>
-        {
-          this.state.fetchInProgress ?
-            <p> Loading from Nebulas blockchain... </p>
-          :
-            <Table>
-              <thead>
-                <tr>
-                  <th>Address</th>
-                  <th>Balance (NAS)</th>
-                </tr>
-              </thead>
-              <tbody>
-                {Members}
-              </tbody>
-            </Table>
-        }
+        {this.state.fetchInProgress ? (
+          <p> Loading from Nebulas blockchain... </p>
+        ) : (
+          <Table>
+            <thead>
+              <tr>
+                <th>Address</th>
+                <th>Balance (NAS)</th>
+              </tr>
+            </thead>
+            <tbody>{Members}</tbody>
+          </Table>
+        )}
       </div>
-    )
+    );
   }
 }
 
-export default Leaderboard
+export default Leaderboard;
